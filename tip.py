b34dd3561fd94c9b423584cc8dbfd0e9f5762a9d
Charlee1904 <charleslee1904@outlook.com>
2022-04-05 21:51:13 -0700

    adding python file + the main questions

---
+++ tip.py
@@ -0,0 +1,9 @@
+print("Tip Calculator")
+bill_total = input("What was the bill total?")
+tip_amount = input("How much would you like to tip? 10, 15, or 20?")
+split = input("how many are in your party?")
+total = bill_total + split
+
+
+
+print(f"Here is your total {total}")
