d070f8ba7f8be285c54ebdc58595e067acf6f9a1
Charlee <Charleslee1904@outlook.com>
2022-04-06 10:02:32 -0700

    made some changes to the logic

--- tip.py
+++ tip.py
@@ -4,6 +4,13 @@ tip_amount = input("How much would you like to tip? 10, 15, or 20?")
 split = input("how many are in your party?")
 total = bill_total + split
 
+bill_total_int = int(bill_total)
+tip_amount_int = int(tip_amount)
 
+tip = tip_amount_int / 100
+x = tip * bill_total_int
+total_with_tip = x + bill_total_int
+
+print(total_with_tip) 
 
 print(f"Here is your total {total}")
